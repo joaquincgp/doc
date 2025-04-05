38670ac321284e6fab9fb57a6ce8bf59feac2ee5
Joaquin Chacon <jchacongp3@gmail.com>
2025-04-05 00:34:22 -0500

    Estructura inicial creada

---
+++ README.md
@@ -0,0 +1,6 @@
+# 🩺 .doc – Plataforma Médica Web
+
+**DOC** es una aplicación web moderna para gestión médica en Ecuador. Incluye directorio de médicos y clínicas, agendamiento de citas, recetas médicas digitales, autenticación con roles y una integración con seguros de salud.
+
+---
+
---
+++ backend/app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
